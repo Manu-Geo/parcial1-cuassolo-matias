8b17fd9f72c27c57fa53618b63bf1cf61147ed58
MaxiCuassolo <maximilianocuassolo2006@gmail.com>
2026-05-20 18:45:11 -0300

    commit casi terminado

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,5 @@
 alert("Bienvenidos a nuestros cursos de programación web");
+console.log("Todo funciona bien");
+document.querySelector('#Boton').onclick= function(){
+    document.body.style.backgroundColor = '#fb4040f7';
+}
